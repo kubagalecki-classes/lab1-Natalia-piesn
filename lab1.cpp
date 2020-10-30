afae58da07c211f6f00440c7462327ce02ea7ac1
Nataliapiesn <01123786@pw.edu.pl>
2020-10-30 08:26:57 +0000

    Task 1 completed

--- lab1.cpp
+++ lab1.cpp
@@ -1,6 +1,20 @@
+#include <cmath>
 #include <iostream>
+using namespace std;
+
+struct Wektor2D
+{
+    double x;
+    double y;
+    void   norm() { cout << sqrt(x * x + y * y) << "\n"; }
+    void   print() { cout << "(" << x << "," << y << ") \n"; }
+};
 
 int main()
 {
-    puts("Hello, World!");
+    Wektor2D vector1;
+    vector1.x = 3.5;
+    vector1.y = 5.5;
+    vector1.norm();
+    vector1.print();
 }
