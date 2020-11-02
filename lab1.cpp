4c33abeba21e4c774727647f03f17806db7ce967
Nataliapiesn <01123786@pw.edu.pl>
2020-11-02 22:06:33 +0000

    Task 5 to fix: * operator

--- lab1.cpp
+++ lab1.cpp
@@ -4,13 +4,20 @@ using namespace std;
 
 class Wektor2D
 {
+
 public:
+    Wektor2D(double a = 0, double b = 0)
+    {
+        x = a;
+        y = b;
+    }
     double x;
     double y;
     void   print() { cout << x << " " << y << endl; }
 
 private:
     friend Wektor2D operator+(const Wektor2D& v1, const Wektor2D& v2);
+    friend Wektor2D operator*(const Wektor2D& v1);
 };
 
 Wektor2D operator+(const Wektor2D& v1, const Wektor2D& v2)
@@ -22,9 +29,17 @@ Wektor2D operator+(const Wektor2D& v1, const Wektor2D& v2)
     return v;
 }
 
+Wektor2D operator*(const Wektor2D& v2, const Wektor2D& v1)
+{
+    return (v2.x * v1.x + v2.y * v1.y);
+}
+
 int main()
 {
-    Wektor2D vec1{3.0, 4.6}, vec2{1.3, 2.8};
+    Wektor2D vec1(3.0, 4.6), vec2(1.3, 2.8);
     Wektor2D v = vec1 + vec2;
     v.print();
+    Wektor2D ilo;
+    ilo = vec1 * vec2;
+    ilo.print();
 }
