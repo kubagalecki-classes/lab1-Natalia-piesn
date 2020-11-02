e07a4c5cd830246b507e681f626692dbd4472cf6
Nataliapiesn <01123786@pw.edu.pl>
2020-11-02 19:52:50 +0000

    Task 4 completed.

--- lab1.cpp
+++ lab1.cpp
@@ -2,33 +2,26 @@
 #include <iostream>
 using namespace std;
 
-struct Informer
-{
-    Informer() { cout << "Constructed! \n"; }
-    ~Informer() { cout << "Deconstructed! \n"; }
-};
-
 struct Wektor2D
 {
-    Wektor2D(double a, double b)
-    {
-        x = a;
-        y = b;
-        cout << a << "," << b << " created\n";
-    }
-    ~Wektor2D() { cout << x << "," << y << " deleted\n"; }
+public:
+    void   setX(double a) { x = a; }
+    double getX() { return x; }
 
-    Informer A;
+    void   setY(double a) { y = a; }
+    double getY() { return y; }
 
+private:
     double x;
     double y;
-    // void   norm() { cout << sqrt(x * x + y * y) << "\n"; }
-    // void   print() { cout << "(" << x << "," << y << ") \n"; }
 };
 
 int main()
 {
-    Wektor2D vector1{3.5, 2.5};
-    Wektor2D vector2{-2, 0.4};
-    //  Wektor2D vector3{-5, 0};
+    Wektor2D vec1;
+    vec1.setX(3.5);
+    vec1.setY(4.5);
+
+    cout << vec1.getX() << " " << vec1.getY() << "\n";
+    // not posiible to run, as x is private: cout << vec1.x;
 }
