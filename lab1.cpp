6e80ea7457f8962d057db86723d79223a8d6c5da
Nataliapiesn <01123786@pw.edu.pl>
2020-11-03 22:53:34 +0000

    Test completed lots of commented text.

--- lab1.cpp
+++ lab1.cpp
@@ -1,7 +1,44 @@
+
 #include <cmath>
 #include <iostream>
 using namespace std;
 
+class Wektor2D
+{
+public:
+    Wektor2D(double a = 0, double b = 0)
+    {
+        x = a;
+        y = b;
+    }
+
+    void   setX(double a) { x = a; }
+    void   setY(double b) { y = b; }
+    double getX() { return x; }
+    double getY() { return y; }
+    ~Wektor2D() {}
+
+private:
+    double          x;
+    double          y;
+    friend Wektor2D operator+(const Wektor2D& v1, const Wektor2D& v2);
+    friend double   operator*(const Wektor2D& v1, const Wektor2D& v2);
+};
+
+Wektor2D operator+(const Wektor2D& v1, const Wektor2D& v2)
+{
+    Wektor2D v = v1;
+    v.x        = v2.x + v1.x;
+    v.y        = v2.y + v1.y;
+    return v;
+}
+
+double operator*(const Wektor2D& v2, const Wektor2D& v1)
+{
+    return v2.x * v1.x + v2.y * v1.y;
+}
+
+/*
 class Wektor2D
 {
 public:
@@ -59,27 +96,45 @@ Wektor2D operator+(const Wektor2D& v1, const Wektor2D& v2)
     return v;
 }
 
-/*
+
 Wektor2D operator*(const Wektor2D& v2, const Wektor2D& v1)
 {
     Wektor2D B;
     B.ilo = v2.x * v1.x + v2.y * v1.y;
     return B;
 }
-*/
-/*
+
+
 std::ostream& operator<<(std::ostream& s, const Wektor2D& v)
 {
     return s << '<' << v.x << ',' << v.y << '>';
 }
 */
+
 int main()
 {
-    cout << Wektor2D::populacja() << "\n";
+    Wektor2D v1{};
+    v1.setX(1.); // setter
+    v1.setY(1.);
+    double x1 = v1.getX(); // getter
+    double y1 = v1.getY();
+
+    cout << x1 << " " << y1 << "\n";
+
+    Wektor2D v2{2., 2.};
+    Wektor2D sum = v1 + v2;
+    cout << v2.getX() << v2.getY() << "\n";
+    cout << sum.getX() << sum.getY() << "\n";
+
+    double pom = v1 * v2;
+    cout << pom << "\n";
+    // double prod = v1 * v2;
+    /*
     Wektor2D A = Wektor2D::kart(3, 0);
     cout << Wektor2D::populacja() << "\n";
     Wektor2D B = Wektor2D::bieg(10, 1);
     // cout << Wektor2D::populacja() << "\n";
     // Wektor2D v1{(1.0, 4.0)};
     // cout << Wektor2D::populacja() << "\n";
+    */
 }
